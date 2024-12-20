ef8246dc560c32b2c826233af87d68fadacf3394
izzyzs <izsoum2024@gmail.com>
2024-12-20 18:11:49 -0500

    all done! :)

--- final_project/urls.py
+++ final_project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from todo.views import home, add_task, edit_task, delete_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home, name='home'),
+    path('add/', add_task, name='add_task'),
+    path('edit/<int:task_id>/', edit_task, name='edit_task'),
+    path('delete/<int:task_id>/', delete_task, name='delete_task'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, 'home.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('/')
+    return render(request, 'add_task.html')
+
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        maybe_title = request.POST.get('title') 
+        maybe_description = request.POST.get('description')
+        if maybe_title != "":
+            task.title = maybe_title 
+        if maybe_description != "":
+            task.description = maybe_description
+        task.completed = 'completed' in request.POST
+        task.save()
+        return redirect('/')
+    return render(request, 'edit_task.html', {'task': task})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('/')
